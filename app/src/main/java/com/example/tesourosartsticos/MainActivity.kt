2632e9cda8d72bace47d9e3ddea27e78f8ed4af4
Vitor <vitorvvc2005@gmail.com>
2024-04-22 20:12:12 -0300

    Subindo intent básica

--- app/src/main/java/com/example/tesourosartsticos/MainActivity.kt
+++ app/src/main/java/com/example/tesourosartsticos/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.tesourosartsticos
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.tela_login)
+        // Intent básica em um botão. Transição de tela
+//        val intent: Intent = Intent(
+//            this,
+//            tela_colecao::class.java
+//        )
+//        startActivity(intent)
     }
+
+
 }
