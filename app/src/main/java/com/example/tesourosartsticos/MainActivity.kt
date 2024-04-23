5a9119dda6c4746c1980ded8f4e08395a91e9ed0
arturbartonelli <arturbartonelli@gmail.com>
2024-04-23 15:59:32 -0300

    First commit
    Bottom menu pronto, com seus respectitvos fragmentos

--- app/src/main/java/com/example/tesourosartsticos/MainActivity.kt
+++ app/src/main/java/com/example/tesourosartsticos/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.tesourosartsticos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.tesourosartsticos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.home -> replaceFragment(Home())
+                R.id.album -> replaceFragment(Album())
+                R.id.camera -> replaceFragment(Camera())
+                R.id.settings -> replaceFragment(Settings())
+
+                else -> {
+
+                }
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment : Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout,fragment)
+        fragmentTransaction.commit()
     }
 }
