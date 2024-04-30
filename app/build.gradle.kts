ad5f06e041ed352ea138ad4ec045a7bd4958f149
Vitor <vitorvvc2005@gmail.com>
2024-04-30 18:09:22 -0300

    Subindo conexão com FireBase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,10 +1,11 @@
-<<<<<<< HEAD
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services") // Adicionado !!
 }
 
 android {
+
     namespace = "com.example.tesourosartsticos"
     compileSdk = 34
 
@@ -43,65 +44,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-=======
-plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-}
 
-android {
-    namespace = "com.example.tesourosartsticos"
-    compileSdk = 34
+    implementation(platform("com.google.firebase:firebase-bom:32.8.1")) // Adicionado!
+    implementation("com.google.firebase:firebase-firestore-ktx:24.11.1") // Adicionado!
 
-    defaultConfig {
-        applicationId = "com.example.tesourosartsticos"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    buildFeatures {
-
-        viewBinding = true
-
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
-}
-
-dependencies {
-
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
->>>>>>> f1e9953e283d7f1ce3edb4968b2c689ad3f9494c
 }
