d989e43752e61d7b414583c54a9248bb0d53670a
Vitor <vitorvvc2005@gmail.com>
2024-05-15 19:46:04 -0300

    Modificando frag de adicionar figurinha e KT

--- app/src/main/java/com/example/tesourosartsticos/quiz.kt
+++ app/src/main/java/com/example/tesourosartsticos/quiz.kt
@@ -5,19 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [quiz.newInstance] factory method to
- * create an instance of this fragment.
- */
 class quiz : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -33,20 +26,24 @@ class quiz : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_quiz, container, false)
+        val view = inflater.inflate(R.layout.fragment_quiz, container, false)
+
+
+        val perguntaQuiz = view.findViewById<EditText>(R.id.perguntaQuiz);
+
+        val opCorreta = view.findViewById<EditText>(R.id.opCorreta);
+        val opErradaUm = view.findViewById<EditText>(R.id.opErradaUm);
+        val opErradaDois = view.findViewById<EditText>(R.id.opErradaDois);
+        val opErradaTres = view.findViewById<EditText>(R.id.opErradaTres);
+
+
+
+
+
+        return view;
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment quiz.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             quiz().apply {
@@ -56,4 +53,6 @@ class quiz : Fragment() {
                 }
             }
     }
+
+
 }
