0d0adbb4187c646fb8f50948c7a89cbfb1bf3792
Vitor <vitorvvc2005@gmail.com>
2024-05-22 19:46:25 -0300

    Subindo códigos para tela de colecão -> Album

--- app/src/main/java/com/example/tesourosartsticos/tela_colecao.kt
+++ app/src/main/java/com/example/tesourosartsticos/tela_colecao.kt
@@ -2,25 +2,24 @@ package com.example.tesourosartsticos
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tesourosartsticos.models.Obra
+import com.example.tesourosartsticos.models.ObrasAdapter
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private const val TAG = "TelaColecaoFragment"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [tela_colecao.newInstance] factory method to
- * create an instance of this fragment.
- */
 class tela_colecao : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -36,27 +35,43 @@ class tela_colecao : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_tela_colecao, container, false)
 
-        val btnMod = view.findViewById<TextView>(R.id.textView11)
+        val btnMod = view.findViewById<Button>(R.id.button)
         btnMod.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.navigateToModificarCarta)
         }
 
+        val db = Firebase.firestore
+        db.collection("Obras")
+            .get()
+            .addOnSuccessListener { result ->
+                val obrasList = mutableListOf<Obra>()
+                for (document in result) {
+                    val nome = document.getString("titulo")
+                    val imageUrl = document.getString("imageUrl")
+                    if (nome != null && imageUrl != null) {
+                        val obra = Obra(nome, imageUrl)
+                        obrasList.add(obra)
+                    }
+                }
+                setupRecyclerView(view, obrasList)
+            }
+            .addOnFailureListener { exception ->
+                Log.e(TAG, "Error getting documents: ", exception)
+            }
+
         return view
     }
 
+    private fun setupRecyclerView(view: View, obrasList: List<Obra>) {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.recycleItems)
+        val adapter = ObrasAdapter(obrasList)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(context)
+    }
+
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment tela_colecao.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             tela_colecao().apply {
