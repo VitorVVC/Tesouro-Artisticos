8ee76e1274882b1712c37f7c12bf91fc554152da
Vitor <vitorvvc2005@gmail.com>
2024-05-22 19:45:38 -0300

    Subindo dependecias novas

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,9 @@ android {
 
 dependencies {
 
+    implementation("com.github.bumptech.glide:glide:4.12.0"); // Adicionado
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0");// Adicionado
+
     implementation("androidx.core:core-ktx:1.13.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
