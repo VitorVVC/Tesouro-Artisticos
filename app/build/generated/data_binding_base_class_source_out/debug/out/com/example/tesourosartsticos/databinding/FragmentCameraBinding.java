54cb448da4a16079ef3e0b18db91dc2cda8c546b
Ricardo Teles <frtcpai@gmail.com>
2024-05-24 18:57:00 -0300

    Alterações de compatibilidade

--- app/build/generated/data_binding_base_class_source_out/debug/out/com/example/tesourosartsticos/databinding/FragmentCameraBinding.java
+++ app/build/generated/data_binding_base_class_source_out/debug/out/com/example/tesourosartsticos/databinding/FragmentCameraBinding.java
@@ -4,38 +4,25 @@ package com.example.tesourosartsticos.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.camera.view.PreviewView;
 import androidx.viewbinding.ViewBinding;
-import androidx.viewbinding.ViewBindings;
 import com.example.tesourosartsticos.R;
 import java.lang.NullPointerException;
 import java.lang.Override;
-import java.lang.String;
 
 public final class FragmentCameraBinding implements ViewBinding {
   @NonNull
-  private final LinearLayout rootView;
+  private final FrameLayout rootView;
 
-  @NonNull
-  public final PreviewView cameraPreview;
-
-  @NonNull
-  public final EditText qrCodeTxt;
-
-  private FragmentCameraBinding(@NonNull LinearLayout rootView, @NonNull PreviewView cameraPreview,
-      @NonNull EditText qrCodeTxt) {
+  private FragmentCameraBinding(@NonNull FrameLayout rootView) {
     this.rootView = rootView;
-    this.cameraPreview = cameraPreview;
-    this.qrCodeTxt = qrCodeTxt;
   }
 
   @Override
   @NonNull
-  public LinearLayout getRoot() {
+  public FrameLayout getRoot() {
     return rootView;
   }
 
@@ -56,25 +43,10 @@ public final class FragmentCameraBinding implements ViewBinding {
 
   @NonNull
   public static FragmentCameraBinding bind(@NonNull View rootView) {
-    // The body of this method is generated in a way you would not otherwise write.
-    // This is done to optimize the compiled bytecode for size and performance.
-    int id;
-    missingId: {
-      id = R.id.cameraPreview;
-      PreviewView cameraPreview = ViewBindings.findChildViewById(rootView, id);
-      if (cameraPreview == null) {
-        break missingId;
-      }
-
-      id = R.id.qrCodeTxt;
-      EditText qrCodeTxt = ViewBindings.findChildViewById(rootView, id);
-      if (qrCodeTxt == null) {
-        break missingId;
-      }
-
-      return new FragmentCameraBinding((LinearLayout) rootView, cameraPreview, qrCodeTxt);
+    if (rootView == null) {
+      throw new NullPointerException("rootView");
     }
-    String missingId = rootView.getResources().getResourceName(id);
-    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
+
+    return new FragmentCameraBinding((FrameLayout) rootView);
   }
 }
