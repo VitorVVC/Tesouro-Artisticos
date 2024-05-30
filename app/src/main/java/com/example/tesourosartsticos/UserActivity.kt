c08bb09e3406e93508aa6b549c4a2c2ff825c90e
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2024-05-30 12:12:11 -0300

    Adicionando userActivity com funcionalidades para perfil

--- app/src/main/java/com/example/tesourosartsticos/UserActivity.kt
+++ app/src/main/java/com/example/tesourosartsticos/UserActivity.kt
@@ -1,27 +1,46 @@
 package com.example.tesourosartsticos
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 //Activity do Usuário
 class UserActivity : AppCompatActivity() {
+
+    private var userPath: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.fragmentContainerView5) as NavHostFragment
+        // Receber o caminho do usuário do Intent
+        userPath = intent.getStringExtra("USER_PATH")
+        if (userPath == null) {
+            Toast.makeText(this, "Erro ao obter caminho do usuário", Toast.LENGTH_SHORT).show()
+            finish()
+            return
+        }
+
+        // Configuração do NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView5) as? NavHostFragment
+                ?: throw IllegalStateException("NavHostFragment não encontrado")
+
         val navController = navHostFragment.navController
-
         val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-
         bottomNavView.setupWithNavController(navController)
 
+        // Passar o caminho do usuário para o fragmento "perfil" quando necessário
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.perfil) {
+                val perfilFragment = Perfil.newInstance(userPath!!)
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragmentContainerView5, perfilFragment)
+                    .addToBackStack(null)
+                    .commit()
+            }
+        }
     }
 }
